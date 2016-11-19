ebf97841df23075c54ca9732219627b562788aa3
Jenni Juvonen <jenni@inbot.io>
2016-11-19 20:03:50 +0100

    Added plenty of dialogs

--- app.js
+++ app.js
@@ -49,3 +49,116 @@ bot.dialog('/product', [
 ]);
 
 intents.onDefault(builder.DialogAction.send("I'm sorry I didn't understand/ je ne comprends pas."));
+
+
+// Jenni's dialogs
+// Note that "session.dialogData.productName" needs to be replaced with values from the database
+
+// Add something to deal with users sending a freetext message instead of picking from the options
+dialog('sell', [
+    // insert logic for checking if there's an entity and checking if we can fetch something from the database
+    session.send("Sure thing! How can I help you?");
+    
+    // Jenni's semi-successful semi-failure of an attempt to build in some logic for dealing with lack of entities and lack of database matches
+    /*function (session) {
+        if ('no item name extracted, there is already something fetched from database') {
+            builder.Prompts.choice(session, "Sure thing! Would you still like to talk about your %s or do you have something else you'd like to sell?", ["%s", "Something else"], session.dialogData.productName);
+        }
+        else if ('no item name extracted, nothing fetched from database') {
+            builder.Prompts.text(session, "Sure thing! What would you like to sell?");
+        }
+        else if ('item name extracted, can be found in database') {
+            session.send("Sure thing! How can I help you?");
+            // exit this dialog
+        }
+        else {
+            session.send("Sorry, I don't have information about that item.");
+            // exit this dialog
+        }  
+    },
+    function (session, results) {
+        if (results.response && results.response.entity == dialogData.productName) {
+            session.send("How can I help you?");
+        } 
+        else if (results.response && results.response.entity == "Something else") {
+            builder.Prompts.text(session, "Sure thing! What would you like to sell?");
+        }
+        else {
+            
+        }
+    }*/
+]);
+
+dialog('price_check', [
+    // insert logic for checking if there's an entity and checking if we can fetch something from the database
+    function (session) {
+        session.send("The item is currently being sold in the range of %s-%s eur and the average price is %s.", session.dialogData.productName);
+    }
+]);
+
+
+dialog('ask_help_description', [
+    // insert logic for checking if there's an entity and checking if we can fetch something from the database
+    function (session) {
+        session.send("Here are the typical keywords others are using in their ads: %s", session.dialogData.productName);
+    }
+]);
+
+
+dialog('current_time_check', [
+    // insert logic for checking if there's an entity and checking if we can fetch something from the database
+    function (session) {
+        if ('no peak time data available') {
+            session.send("Sorry, I couldn't find enough sales data for this item. It's probably new or rare :)");
+        }
+        else if ('current month is not peak month or non peak month') {
+            if ('there is one peak month'){
+                session.send("Now is not a bad time to sell, but %s would be even better.", session.dialogData.productName);
+            }
+            else {
+                session.send("Now is not a bad time to sell, but %s and %s would be better.", session.dialogData.productName);
+            }
+        }
+        else if ('current month is peak month') {
+            session.send("Hmm, it looks like a lot of people are selling it right now. There would be less competition in %s.", session.dialogData.productName);
+        }
+        else if ('current month is non peak month') {
+            session.send("Now would be a great time to sell, there's less competition than usually!", session.dialogData.productName);
+        }
+        else {
+            session.send("Hmm, that didn't work. Could I help you with something else?");
+        }
+    }
+]);
+
+
+dialog('q_optimal_time', [
+    // insert logic for checking if there's an entity and checking if we can fetch something from the database
+    function (session) {
+        if ('one non-peak month') {
+            session.send("%s would be the best month to sell this item.", session.dialogData.productName);
+        }
+        else {
+            session.send("%s and %s would be the best months to sell this item.", session.dialogData.productName);
+        }
+    }
+]);
+
+
+// Add something to deal with users sending a freetext message instead of picking from the options
+dialog('end', [
+    // insert logic for checking if there's an entity and checking if we can fetch something from the database
+    function (session) {
+        builder.Prompts.choice(session, "Okay. Are you selling anything else?", ["Yes", "No"]);
+    },
+    function (session, results) {
+        if (results.response && results.response.entity == 'Yes') {
+            // clear the current conversation context
+            session.beginDialog('sell');
+        } else {
+            session.send("Alright, let me know when you do. Talk to you later!");
+        }
+    }
+]);
+
+
